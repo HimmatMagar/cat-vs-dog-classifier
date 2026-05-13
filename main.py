a7eb3692ff122d77335c88d61b7559ffbd7cf3a4
Himmat Magar <himmatmagar007@gmail.com>
2026-05-13 20:19:27 +0545

    changes in pipeline

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from src.Classifier.pipeline.data_ingestion_pipeline import DataIngestionPipelin
 from src.Classifier.pipeline.model_training_pipeline import ModelTrainingPipeline
 from src.Classifier.pipeline.model_eval_pipeline import ModelEvalPipeline
 
-# STAGE_NAME = "Data Ingestion Stage"
-# try:
-#       logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
-#       obj = DataIngestionPipeline()
-#       obj.main()
-#       logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
-# except Exception as e:
-#       logger.exception(e)
-#       raise e
+STAGE_NAME = "Data Ingestion Stage"
+try:
+      logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+      obj = DataIngestionPipeline()
+      obj.main()
+      logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+      logger.exception(e)
+      raise e
 
 
-# stage_name = "Model Training Stage"
-# try:
-#       logger.info(f">>>>>> {stage_name} started <<<<<<")
-#       obj = ModelTrainingPipeline()
-#       obj.Train_Model()
-#       logger.info(f">>>>>> {stage_name} completed <<<<<<")
-# except Exception as e:
-#       logger.exception(e)
-#       raise e
+stage_name = "Model Training Stage"
+try:
+      logger.info(f">>>>>> {stage_name} started <<<<<<")
+      obj = ModelTrainingPipeline()
+      obj.Train_Model()
+      logger.info(f">>>>>> {stage_name} completed <<<<<<")
+except Exception as e:
+      logger.exception(e)
+      raise e
 
 
 STAGE_NAME = "Model Eval Stage"
